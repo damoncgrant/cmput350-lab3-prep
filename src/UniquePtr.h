0e81c6548751b13c321b1932d28f41c0373afbaa
Damon Grant <dcgrant@ualberta.ca>
2026-09-15 12:08:36 -0600

    Finished lab prep

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -1,6 +1,89 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
+#include <utility>
 
 // Your implementation here
+template <typename T>
+class UniquePtr {
+    public: 
+        UniquePtr(T* p = nullptr) : ptr(p)  // ctor
+        {
+        }
 
+        ~UniquePtr() {  // dtor
+            if (ptr != nullptr) {
+                delete ptr;
+            }
+        }
+        
+        UniquePtr(UniquePtr&& other) : ptr(other.ptr)  // Move Constructor
+        {
+            other.ptr = nullptr;
+        }
+        
+        UniquePtr& operator=(UniquePtr&& other) // Move assignment
+        {
+            std::swap(ptr, other.ptr);
+            return *this;
+        }
+
+        // Copy not allowed
+        UniquePtr(const UniquePtr& other) = delete; // Copy Constructor
+        UniquePtr& operator=(const UniquePtr&) = delete;  // Copy assignment
+
+        template <typename U>
+        UniquePtr(UniquePtr<U>&& other) : ptr(other.release())  // Converting constructor
+        {
+        }
+
+        T& operator*() const    // Dereference operator
+        {
+            return *ptr;
+        }
+
+        T* operator->() const   // Arrow operator
+        {
+            return ptr;
+        }
+
+        T* get() const  // get(), the same as the arrow operator
+        {
+            return ptr;
+        }
+
+        bool operator==(const UniquePtr<T>& other) const // Comparison operator
+        {
+            return other.ptr == ptr;
+        }
+
+        T* release()  // Release operator
+        {
+            T* temp = ptr;
+            ptr = nullptr;
+            return temp;
+        }
+
+        void reset(T* newPtr = nullptr) // Reset operator
+        {
+            T* temp = ptr;
+            ptr = newPtr;
+            delete temp;
+        }
+
+        void swap(UniquePtr<T>& other)  // Swap operator
+        {
+            T* temp = other.ptr;
+            other.ptr = ptr;
+            ptr = temp;
+        }
+
+        operator bool() const   // Bool operator -> returns true if ptr is non-empty
+        {
+            return ptr != nullptr;
+        }
+
+    private:
+        T* ptr;
+
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include "UniquePtr.h"
+#include <cassert>
+#include <utility>
+#include <iostream>
 
 int main() {
-    // UniquePtr<int> ptr(new int(5));
-    // they test all methods here...
+    UniquePtr<int> ptr(new int(5));
+    UniquePtr<int> ptr2(new int(10));
+    UniquePtr<int> empty;
+    assert(*ptr == 5);
+    assert(bool(ptr));
+    assert(!empty);
+    assert(ptr.get() == &*ptr);
+    assert(!(ptr == ptr2));
+
+    // Move constructor and assignment.
+    UniquePtr<int> ptr3(std::move(ptr));
+    assert(*ptr3 == 5 && !ptr);
+    ptr2 = std::move(ptr3);
+    assert(*ptr2 == 5);
+
+    // Converting constructor.
+    UniquePtr<const int> converted(std::move(ptr2));
+    assert(*converted == 5);
+
+    int* released = ptr3.release();
+    assert(*released == 10 && !ptr3);
+
+    ptr.reset(new int(20));
+    assert(*ptr == 20);
+    ptr.swap(empty);
+    assert(!ptr && *empty == 20);
+    empty.reset();
+    assert(!empty);
+
+    std::cout << "All assertions passed" << std::endl;
+
     return 0;
 }
